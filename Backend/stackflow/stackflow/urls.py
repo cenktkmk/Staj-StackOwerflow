5b77758c9033f931dd2b29f75a0305be66a5f2b0
Cenktkmk <116560930+cenktkmk@users.noreply.github.com>
2023-10-09 16:32:56 +0300

    Add files via upload

--- Backend/stackflow/stackflow/urls.py
+++ Backend/stackflow/stackflow/urls.py
@@ -1,19 +1,4 @@
-"""
-URL configuration for stackflow project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from userapp.views import *
@@ -23,6 +8,12 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", index, name="home"),
+    path("profile", profile, name="profile"),
+    path("questions", questions, name="questions"),
+    path("questions/Detail", questionDetails, name="questionDetails"),
+    path("tags", tags, name="tags"),
+    path("users", users, name="users")
+
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
