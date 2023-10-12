097c32913a0ee2e09f6ce47afbd786d743bcb0f3
Talhann <ozturktalhan@gmail.com>
2023-10-12 15:33:31 +0300

    kk

--- userapp/form.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import *
-
-class RegisterForm(forms.ModelForm):
-    class Meta:
-        model = UserModel
-        fields = ["username", "email", "password"]
